3cdaa9e267ae50b19595987fa8cbbdc87a31174a
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-05-05 21:01:46 +0200

    chart added

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ class App extends React.Component {
   };
 
   async componentDidMount() {
-    const fetchedData = await fetchData();
+    const data = await fetchData();
 
-    this.setState = { data: fetchedData };
+    this.setState = { data };
+    console.log("app", data.confirmed);
   }
 
   render() {
